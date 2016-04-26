b55290584212abe747390e148ba7cc4710a873fa
Sebastiaan Luca <hello@sebastiaanluca.com>
2016-04-26 18:09:21 +0200

    Set up Mockery and add PHPDoc to default test case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,15 +2,43 @@
 
 namespace SebastiaanLuca\Router\Tests;
 
+use Mockery;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
 use SebastiaanLuca\Router\RouterServiceProvider;
 
 class TestCase extends OrchestraTestCase
 {
     
+    use MockeryPHPUnitIntegration;
+    
+    /**
+     * Get package providers.
+     *
+     * @param \Illuminate\Foundation\Application $app
+     *
+     * @return array
+     */
     protected function getPackageProviders($app)
     {
         return [RouterServiceProvider::class];
+    }
+    
+    /**
+     * Mock a class and bind it in the IoC container.
+     *
+     * @param string $class
+     * @param mixed $parameters
+     *
+     * @return \Mockery\MockInterface
+     */
+    protected function mock($class, $parameters = [])
+    {
+        $mock = Mockery::mock($class, $parameters);
+        
+        $this->app->instance($class, $mock);
+        
+        return $mock;
     }
     
 }
