76474588af224d0cae935afe292caae53a83c22f
Sebastiaan Luca <hello@sebastiaanluca.com>
2016-05-04 22:58:41 +0200

    Remove optional service provider aliasing

--- src/RouterServiceProvider.php
+++ src/RouterServiceProvider.php
@@ -58,9 +58,6 @@ class RouterServiceProvider extends RouteServiceProvider
         
         // Swap the default router with our extended router
         $this->app->alias(ExtendedRouter::class, 'router');
-        
-        // This is optional, but you never know
-        $this->app->alias(ExtendedRouter::class, Router::class);
     }
     
     /**
