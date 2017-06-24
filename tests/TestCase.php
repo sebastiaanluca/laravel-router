5098130ff40a34fb2f7dcbfcb221e14895056617
Sebastiaan Luca <hello@sebastiaanluca.com>
2017-06-24 13:05:47 +0200

    Clean up test case
    Signed-off-by: Sebastiaan Luca <hello@sebastiaanluca.com>

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,7 +6,6 @@ use Mockery;
 use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 use Mockery\MockInterface;
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
-use ReflectionClass;
 use SebastiaanLuca\Router\RouterServiceProvider;
 
 class TestCase extends OrchestraTestCase
@@ -40,62 +39,5 @@ class TestCase extends OrchestraTestCase
         $this->app->instance($class, $mock);
 
         return $mock;
-    }
-
-    /**
-     * Sets a private or protected class method to be publicly accessible.
-     *
-     * @param mixed $class
-     * @param string $name
-     *
-     * @return \ReflectionMethod
-     * @deprecated If you use this, your tests are probably bad.
-     */
-    protected function enablePublicAccessOfMethod($class, $name)
-    {
-        $reflection = new ReflectionClass($class);
-        $method = $reflection->getMethod($name);
-
-        $method->setAccessible(true);
-
-        return $method;
-    }
-
-    /**
-     * Sets a private or protected class property to be publicly accessible.
-     *
-     * @param $class
-     * @param $property
-     *
-     * @return \ReflectionProperty
-     * @deprecated If you use this, your tests are probably bad.
-     */
-    protected function enablePublicAccessOfProperty($class, $property)
-    {
-        $reflection = new ReflectionClass($class);
-        $property = $reflection->getProperty($property);
-
-        $property->setAccessible(true);
-
-        return $property;
-    }
-
-    /**
-     * Set the value of a private or protected class property.
-     *
-     * @param object $instance
-     * @param string $property
-     * @param mixed $value
-     *
-     * @return \ReflectionProperty
-     * @deprecated If you use this, your tests are probably bad.
-     */
-    protected function setValueOfInternalProperty($instance, $property, $value)
-    {
-        $property = $this->enablePublicAccessOfProperty($instance, $property);
-
-        $property->setValue($instance, $value);
-
-        return $property;
     }
 }
